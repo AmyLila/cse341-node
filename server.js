1238cc864bffcc60525958fe36682b580a48c9e0
shakerbaker78 <bakeamym@yahoo.com>
2022-04-22 14:24:04 -0600

    trying again

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+// app.use('/', require('./routes'))
+
+app.get('/', (req, res) => {
+  res.send('Brady Baker!')
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`)
+})
