db588bb45427ca7f895d441f7c122996e554bc78
shakerbaker78 <bakeamym@yahoo.com>
2022-04-28 21:51:09 -0600

    I don't even know what I'm doing. Trying to get mongodb to connect

--- utilities/database.js
+++ utilities/database.js
@@ -5,10 +5,11 @@ const MongoUser = mongodb.MongoUser;
 //Connection Method. I learned this from a udemy course. It uses the mongodb url to connect and then logs a success or error in the console. 
 //https://www.udemy.com/course/nodejs-the-complete-guide/learn/lecture/11942758#overview
 const mongoConnection = (callback) => {
-    MongoUser.connect('mongodb+srv://AmyLilaBaker:qblEihLJDcifnt0r@cse341.e1a0s.mongodb.net/clients?retryWrites=true&w=majority')
-        .then(result => {
+    MongoUser.connect('mongodb+srv://AmyLilaBaker:qblEihLJDcifnt0r@cse341.e1a0s.mongodb.net/contacts?retryWrites=true&w=majority'
+    )
+        .then(user => {
             console.log('Connected!');
-            callback(result);
+            callback(user);
         })
         .catch(err =>{
             console.log(err)});
@@ -19,7 +20,7 @@ module.exports = mongoConnection;
 
 
 // This is where I'm trying to call my password from a the hidden_stuff file. 
-MongoUser.connect('mongodb+srv://AmyLilaBaker:qblEihLJDcifnt0r@cse341.e1a0s.mongodb.net/clients?retryWrites=true&w=majority');
+// MongoUser.connect('mongodb+srv://AmyLilaBaker:qblEihLJDcifnt0r@cse341.e1a0s.mongodb.net/contacts?retryWrites=true&w=majority');
 
 
 
