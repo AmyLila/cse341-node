e997a8bb7a868e89ca9c8c710a4968ffe8c668d2
shakerbaker78 <bakeamym@yahoo.com>
2022-05-05 10:16:42 -0600

    created a practice folder to learn CRUD

---
+++ routes/learnCRUD.js
@@ -0,0 +1,34 @@
+const routes = require('express').Router();
+const connection = require('../database/connection');
+const ObjectId = require('mongodb').ObjectId;
+
+// code to get all contacs
+routes.get('/', (req, res) => {
+
+    const resultsAll = connection.getCollection().find();
+    resultsAll.toArray().then((documents) => {
+        res.status(200).json(documents);
+        console.log('All Contacts! From the contacts file.')
+
+
+    });
+    
+
+}); //end get all contacts
+
+
+//Get contact by ID
+routes.get('/:id', (req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const resultsAll = connection.getCollection().find({_id: contactId});
+    resultsAll.toArray().then((documents) => {
+        res.status(200).json(documents[0]);
+        console.log(`One Contact: ${req.params.id}! From the contacts file.`)
+
+
+    });
+    
+
+}); //End get contacts by ID
+
+module.exports = routes  
