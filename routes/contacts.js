f0bdaf824b367c3d4f14fdcdabd7c1f07d1ce098
shakerbaker78 <bakeamym@yahoo.com>
2022-05-07 18:51:52 -0600

    added my week three work to the contact file.

--- routes/contacts.js
+++ routes/contacts.js
@@ -33,4 +33,65 @@ routes.get('/:id', (req, res) => {
 
 }); //End get contacts by ID
 
+
+
+//POST route to create a new contact
+routes.post('/', (req, res) => {
+    
+    const newContact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+  };
+
+    const result = connection.getCollection().insertOne(newContact);
+    result.then((documents) => {
+      res.status(201).json(documents);
+
+    });
+    console.log(result);
+
+
+
+}); //End create new contact
+
+
+//Code to modify an existing contact by id
+routes.put('/:id', (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  const result = connection.getCollection().replaceOne({_id: contactId}, contact);
+  result.then((documents) => {
+    res.status(202).json(documents);
+
+  });
+  
+  console.log(contact);
+ 
+});//end modify contact
+
+
+//Delete contact by id
+routes.delete('/:id', (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const result = connection.getCollection().deleteOne({_id:contactId});
+  result.then((documents) => {
+    res.status(202).json(documents);
+
+  });
+  console.log(result);
+  
+
+
+});
+
 module.exports = routes  
